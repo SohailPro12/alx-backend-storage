2b35cbc355d665ca62361a779d1a40011457fb25
Sohail Charef <charefsohayl@gmail.com>
2024-07-18 10:27:05 +0000

    task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+exercise.py module
+"""
+import redis
+import uuid
+
+
+class Cache:
+    """Represents an object for storing data in a Redis data storage.
+    """
+    def __init__(self) -> None:
+        """Initializes a Cache instance.
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb(True)
+
+    def store(self, data: str) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
